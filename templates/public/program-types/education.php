c4a7349156352ffeefbe5a2b04cb795d3312ed43
System Administrator <root@Abrehams-MBP.lan>
2025-05-20 22:14:12 -0500

    Program View Page 6

--- templates/public/program-types/education.php
+++ templates/public/program-types/education.php
@@ -1,7 +1,25 @@
-<div class="program-type-education">
-    <h2>Education Program View</h2>
-    <div class="program-data">
+<div class="program-view-edu-layout">
+  <div class="program-view-edu-top-row">
+    <div class="program-view-edu-overview card">
+      <button class="program-view-edu-edit-btn" title="Edit Program"><span class="dashicons dashicons-edit"></span></button>
+      <h3 class="program-view-edu-title">Overview Info</h3>
+      <div class="program-view-edu-overview-content program-data">
         <!-- Program-specific data will be injected here -->
+      </div>
     </div>
-    <div class="program-courses-list-placeholder">[Courses list will appear here]</div>
+    <div class="program-view-edu-courses card">
+      <button class="program-view-edu-add-course-btn" title="Add Course"><span class="dashicons dashicons-plus"></span></button>
+      <h3 class="program-view-edu-title">Courses</h3>
+      <div class="program-view-edu-courses-list">
+        <div class="program-courses-list-placeholder">[Courses list will appear here]</div>
+      </div>
+    </div>
+  </div>
+  <div class="program-view-edu-enrollment card">
+    <button class="program-view-edu-add-enrollment-btn" title="Add Enrollment"><span class="dashicons dashicons-plus"></span></button>
+    <h3 class="program-view-edu-title">Enrollment</h3>
+    <div class="program-view-edu-enrollment-content">
+      <div class="program-enrollment-list-placeholder">[Enrollment info will appear here]</div>
+    </div>
+  </div>
 </div> 
